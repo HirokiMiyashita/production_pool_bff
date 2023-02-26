e16897a487f4be5089ccd5c4bdafa24f2ef07f6a
M-Hirok <hiroki.miyashita.0215@gmail.com>
2023-02-27 05:07:12 +0900

    wip

--- src/action/_common/cognitoAccess.ts
+++ src/action/_common/cognitoAccess.ts
@@ -1,10 +1,10 @@
-// import { UserPool } from "aws-cdk-lib/aws-cognito";
+// import { UserPool } from 'aws-cdk-lib/aws-cognito';
 import {
   AdminCreateUserCommandOutput,
   CognitoIdentityProvider,
-} from "@aws-sdk/client-cognito-identity-provider";
-// import { aws_cognito } from "aws-cdk-lib";
-// import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
+} from '@aws-sdk/client-cognito-identity-provider';
+// import { aws_cognito } from 'aws-cdk-lib';
+// import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 
 // interface signUpProps {
 //   email: string;
@@ -20,32 +20,31 @@ export const createCognitoUser =
 
     try {
       const userCommandOutput = await cognito.adminCreateUser({
-        UserPoolId: "ap-northeast-1_mOgcJakUe",
-        MessageAction: "SUPPRESS",
-        Username: "hirokixyzrr@gmail.com",
-        TemporaryPassword: "111AAAaaa",
+        UserPoolId: 'ap-northeast-1_mOgcJakUe',
+        Username: 'hirokixyzrr@gmail.com',
+        MessageAction: 'SUPPRESS',
         UserAttributes: [
           {
-            Name: "email",
-            Value: "hirokixyzrr@gmail.com",
+            Name: 'email',
+            Value: 'hirokixyzrr@gmail.com',
           },
           {
-            Name: "email_verified",
-            Value: "true",
+            Name: 'email_verified',
+            Value: 'true',
           },
         ],
       });
 
       await cognito.adminSetUserPassword({
-        UserPoolId: "ap-northeast-1_mOgcJakUe",
+        UserPoolId: 'ap-northeast-1_mOgcJakUe',
         Username: userCommandOutput.User?.Username,
-        Password: "111AAAaaa",
+        Password: '111AAAaaa',
         Permanent: true,
       });
 
       return userCommandOutput;
     } finally {
-      console.debug("end");
+      console.debug('end');
     }
   };
 
@@ -54,18 +53,18 @@ export const createCognitoUser =
 //     region: process.env.AWS_REGION,
 //   });
 //   const poolData = {
-//     ClientId: "gou0d50er22te9jkccn664cak",
-//     Username: "hirokixyzrr@gmail.com",
-//     Password: "111AAAaaa",
+//     ClientId: 'gou0d50er22te9jkccn664cak',
+//     Username: 'hirokixyzrr@gmail.com',
+//     Password: '111AAAaaa',
 //   };
 //   // var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
 //   cognito.signUp(poolData, function (err, data) {
 //     if (err) {
-//       console.error("サインアップに失敗しました", data);
+//       console.error('サインアップに失敗しました', data);
 //       return err;
 //     } else {
-//       console.debug("サインアップに失敗しました", data);
+//       console.debug('サインアップに失敗しました', data);
 //       return data;
 //     }
 //   });
